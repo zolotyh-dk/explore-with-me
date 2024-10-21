4cb161c4ba64cbab1c343952c6a94ca65adc1f66
Andrei Kuniutka <akuniutka@gmail.com>
2024-10-21 23:04:55 +0300

    Fix: Create stub controller for stats service

---
+++ stats/stats-service/src/main/java/ru/practicum/ewm/TestController.java
@@ -0,0 +1,15 @@
+package ru.practicum.ewm;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class TestController {
+
+    @GetMapping
+    String hello() {
+        return "Hello";
+    }
+}
