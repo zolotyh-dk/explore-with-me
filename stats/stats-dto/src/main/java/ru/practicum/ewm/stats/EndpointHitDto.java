b051dd23f1b4d289be7f7a5f3579aad773c2ea96
Andrei Kuniutka <akuniutka@gmail.com>
2024-10-22 11:55:27 +0300

    Fix: Add stats DTO field constraints

--- stats/stats-dto/src/main/java/ru/practicum/ewm/stats/EndpointHitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/ewm/stats/EndpointHitDto.java
@@ -1,5 +1,9 @@
 package ru.practicum.ewm.stats;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -7,9 +11,20 @@ import java.time.LocalDateTime;
 @Builder(toBuilder = true)
 public record EndpointHitDto(
 
+        @NotBlank
+        @Size(max = 255)
         String app,
+
+        @NotBlank
+        @Size(max = 512)
         String uri,
+
+        @NotBlank
+        @Size(max = 40)
         String api,
+
+        @NotNull
+        @PastOrPresent
         LocalDateTime timestamp) {
 
 }
